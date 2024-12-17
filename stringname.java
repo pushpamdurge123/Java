614255726aeaa513fc5d00703bff888909f16c46
Pushpam Durge <pushpam664@gmail.com>
2024-12-18 01:49:44 +0530

    Add files via upload

---
+++ stringname.java
@@ -0,0 +1,16 @@
+//day5
+public class stringname {
+
+	public static void main(String[] args) {
+		
+		String name = "I am ram";
+		
+		String name1 = "I am ram";
+		
+		String name2 = new String("I am ram");
+		
+		System.out.println(name == name2);
+
+	}
+
+}
