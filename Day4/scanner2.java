e2be08bdbdfb1075037e954b98d69e078dde7a7b
Pushpam Durge <pushpam664@gmail.com>
2024-12-17 22:52:58 +0530

    Add files via upload

---
+++ Day4/scanner2.java
@@ -0,0 +1,22 @@
+package Day4;
+
+public class scanner2 {
+	
+//	int add(int a,int b) {
+//		return a + b;
+//	}
+	
+	void add(int a,int b) {
+		System.out.println("Result: " + (a + b));
+	}
+
+	public static void main(String[] args) {
+		
+		scanner2 obj = new scanner2();
+		obj.add(10, 20);
+//		int result = obj.add(10, 30);
+//		System.out.println(result);
+
+	}
+
+}
