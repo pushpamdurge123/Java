aeebdc7abe8d5b7f7580c27cd78e4448824f4796
Pushpam Durge <pushpam664@gmail.com>
2024-12-19 01:35:12 +0530

    Add files via upload

---
+++ stringStartwith.java
@@ -0,0 +1,13 @@
+
+public class stringStartwith {
+
+	public static void main(String[] args) {
+		
+		String a = "Sachin";
+		
+		System.out.println(a.startsWith("S"));
+		System.out.println(a.endsWith("n"));
+
+	}
+
+}
