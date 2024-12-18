0823421499850d5bf6b01d86106a05df252ff26f
Pushpam Durge <pushpam664@gmail.com>
2024-12-19 01:18:15 +0530

    Add files via upload

---
+++ stringCases.java
@@ -0,0 +1,13 @@
+
+public class stringCases {
+
+	public static void main(String[] args) {
+		
+		String a = "Hello";
+		
+		System.out.println(a.toLowerCase());
+		System.out.println(a.toUpperCase());
+
+	}
+
+}
