41a1c3472c1107baf355db62d6a5fa69b6172756
Pushpam Durge <pushpam664@gmail.com>
2024-12-19 19:26:59 +0530

    Add files via upload

---
+++ stringconcat2.java
@@ -0,0 +1,18 @@
+
+public class stringconcat2 {
+
+	public static void main(String[] args) {
+		
+		String a =50+30+"Sachin"+40+40;
+		System.out.println(a);
+		
+		String s1 ="Sachin";
+		String s2 =" Tendulkar";
+		String s3=s1.concat(s2);
+		 System.out.println(s3);
+		
+		
+
+	}
+
+}
