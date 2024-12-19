03bde6f5fd97bb6aaaf39bd854f8ed7c014cd08c
Pushpam Durge <pushpam664@gmail.com>
2024-12-19 21:42:23 +0530

    Add files via upload

---
+++ stringcontains.java
@@ -0,0 +1,12 @@
+
+public class stringcontains {
+
+	public static void main(String[] args) {
+		
+		String s = "Hello Hi How Are You";
+		
+		System.out.println(s.contains("Hi How Are"));
+
+	}
+
+}
