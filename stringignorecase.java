433777d7b59f045e63b17721a0659de4e6764bef
Pushpam Durge <pushpam664@gmail.com>
2024-12-19 18:48:56 +0530

    Add files via upload

---
+++ stringignorecase.java
@@ -0,0 +1,16 @@
+
+public class stringignorecase {
+
+	public static void main(String[] args) {
+		
+		String a = "Hello World";
+		
+		String b = "Hello World";
+		
+		String c = "hello World";
+		
+		System.out.println(a.equalsIgnoreCase(c));
+
+	}
+
+}
