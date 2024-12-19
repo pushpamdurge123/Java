956b8d5797da829de05de18efacd21bdeb041adf
Pushpam Durge <pushpam664@gmail.com>
2024-12-19 22:05:48 +0530

    Add files via upload

---
+++ valueofmethod.java
@@ -0,0 +1,16 @@
+
+public class valueofmethod {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		String b = String.valueOf(a);
+		
+		System.out.println(a+a);
+		
+		System.out.println(b+b);
+
+	}
+
+}
