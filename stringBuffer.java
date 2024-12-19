7b76f4a6a33bd59df6156b4283113ea6bd51240e
Pushpam Durge <pushpam664@gmail.com>
2024-12-20 00:46:20 +0530

    Add files via upload

---
+++ stringBuffer.java
@@ -0,0 +1,13 @@
+
+public class stringBuffer {
+
+	public static void main(String[] args) {
+		
+		StringBuffer buffer =  new StringBuffer("Hello");
+		
+		buffer.append(" World");
+		System.out.println(buffer);
+
+	}
+
+}
