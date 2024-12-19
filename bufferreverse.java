05804d3d7b5d67caed6d6beac4a2d07f3228ba3c
Pushpam Durge <pushpam664@gmail.com>
2024-12-20 01:30:28 +0530

    Add files via upload

---
+++ bufferreverse.java
@@ -0,0 +1,13 @@
+
+public class bufferreverse {
+
+	public static void main(String[] args) {
+		
+		StringBuffer buffer = new StringBuffer("Hello");
+
+		
+		System.out.println(buffer.reverse());
+
+	}
+
+}
