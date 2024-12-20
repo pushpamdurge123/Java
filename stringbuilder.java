145c36f14c9c06a840170a9b72f3ef92f4aabc09
Pushpam Durge <pushpam664@gmail.com>
2024-12-21 00:55:53 +0530

    Add files via upload

---
+++ stringbuilder.java
@@ -0,0 +1,13 @@
+
+public class stringbuilder {
+
+	public static void main(String[] args) {
+		
+		StringBuilder builder = new StringBuilder("Hello");
+
+		
+		System.out.println(builder.append(" Wolrd"));
+
+	}
+
+}
