ac4ba43e97010914dc90b04686de62ed9e117479
Pushpam Durge <pushpam664@gmail.com>
2024-12-20 23:04:33 +0530

    Add files via upload

---
+++ buffercapacity.java
@@ -0,0 +1,13 @@
+
+public class buffercapacity {
+
+	public static void main(String[] args) {
+		
+		StringBuffer buffer = new StringBuffer("Hello Wolrd");
+
+		
+		System.out.println(buffer.capacity());
+
+	}
+
+}
