ea1863bd5de6e0a64563f69fa7b0bf2e81e47ca5
Pushpam Durge <pushpam664@gmail.com>
2024-12-21 00:51:01 +0530

    Add files via upload

---
+++ parsefloat.java
@@ -0,0 +1,15 @@
+
+public class parsefloat {
+
+	public static void main(String[] args) {
+		
+		String a ="20";
+		
+		float b = Float.parseFloat(a);
+		
+		System.out.println(50+50+b+b+50+50);
+		
+
+	}
+
+}
