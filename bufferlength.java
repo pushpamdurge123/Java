50376254365a8d28174cb5b3af90fb7b6c412a3f
Pushpam Durge <pushpam664@gmail.com>
2024-12-20 22:52:04 +0530

    Add files via upload

---
+++ bufferlength.java
@@ -0,0 +1,13 @@
+
+public class bufferlength {
+
+	public static void main(String[] args) {
+		
+		StringBuffer buffer = new StringBuffer("Hello Wolrd");
+
+		
+		System.out.println(buffer.length());
+
+	}
+
+}
